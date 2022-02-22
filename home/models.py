0fc6215117c33fa8202617193d3a159bf296cfbf
DavidPerezVelilla <dvdpv926@gmail.com>
2022-02-22 11:48:19 +0100

    1

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class HomePage(Page):
@@ -11,3 +12,46 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        noticias = noticia.objects.all()[:2]
+        context['noticias'] = noticias
+        return context
+
+class noticia (models.Model):
+    titulo = models.CharField('titulo', max_length=250)
+    contenido = models.CharField('contenido', max_length=1000)
+
+    panels = [
+        FieldPanel('titulo'),
+        FieldPanel('contenido')
+
+    ]
+    def __str__(self):
+        return f'{self.titulo}'
+    
+    class Meta:
+        verbose_name = 'Noticia'
+        verbose_name_plural = 'Noticias'
+
+class NoticiaIndexPage(Page):
+    introduccion = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduccion', classname="full")
+    ]
+
+    def paginate(self, request, noticia, *args):
+        page = request.GET.get('page')
+        
+        paginator = Paginator(noticia, 15)
+        try:
+            pages = paginator.page(page)
+        except PageNotAnInteger:
+            pages = paginator.page(1)
+        except EmptyPage:
+            pages = paginator.page(paginator.num_pages)
+        return pages
+
+   
---
+++ home/wagtail_hooks.py
@@ -0,0 +1,43 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, ModelAdminGroup, modeladmin_register)
+
+from home.models import noticia
+
+'''
+N.B. To see what icons are available for use in Wagtail menus and StreamField block types,
+enable the styleguide in settings:
+
+INSTALLED_APPS = (
+   ...
+   'wagtail.contrib.styleguide',
+   ...
+)
+
+or see http://kave.github.io/general/2015/12/06/wagtail-streamfield-icons.html
+
+This demo project includes the full font-awesome set via CDN in base.html, so the entire
+font-awesome icon set is available to you. Options are at http://fontawesome.io/icons/.
+'''
+
+class NoticiaAdmin(ModelAdmin):
+    # These stub classes allow us to put various models into the custom "Wagtail Bakery" menu item
+    # rather than under the default Snippets section.
+    model = noticia
+    search_fields = ('titulo')
+    menu_icon = 'fa-tags'  # change as required
+    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
+
+
+
+class NoticiaAdminGroup(ModelAdminGroup):
+    # These stub classes allow us to put various models into the custom "Wagtail Bakery" menu item
+    # rather than under the default Snippets section.
+    menu_label = 'noticia'
+    menu_icon = 'fa-tags'  # change as required
+    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
+    items = (NoticiaAdmin,)
+
+
+# When using a ModelAdminGroup class to group several ModelAdmin classes together,
+# you only need to register the ModelAdminGroup class with Wagtail:
+modeladmin_register(NoticiaAdminGroup)
